2083c7b06c27ce0bfbdfc83daf56d7127c652ea2
Dave Wilson <dave@webartz.com>
2022-01-15 13:36:31 -0600

    Invert right motor direction and remove redundant tuning params

--- src/main/java/frc/robot/TuningParams.java
+++ src/main/java/frc/robot/TuningParams.java
@@ -44,68 +44,6 @@ public final class TuningParams
 
     public static final double  DEADBAND_TURN                   = 0.01;
 
-    //////////////////////////////
-    // Color Wheel Params
-    //////////////////////////////
-    public static final Color RGB_CYAN                         = new Color(0.1799, 0.4451, 0.3799);
-    public static final Color RGB_GREEN                        = new Color(0.2199, 0.5200, 0.2300);
-    public static final Color RGB_RED                          = new Color(0.4199, 0.3899, 0.1699);
-    public static final Color RGB_YELLOW                       = new Color(0.3100, 0.5300, 0.1499);
-
-    public static final int COLOR_WHEEL_PROXIMITY_THRESHOLD    = 30;
-
-    public static final double COLOR_WHEEL_SPEED               = 0.5; 
-
-    public static final int COLOR_WHEEL_TRANSITIONS            = 26; // Slightly more than three revs
-
-    public static final int COLOR_WHEEL_ARRAY_SIZE             = 3;
-
-    //////////////////////////////
-    // Launcher Params
-    //////////////////////////////
-    public static final double LOADER_MAX_SPEED                = 1.0;
-    public static final double RELEASE_MOTOR_SPEED             = 1.0;
-    // TODO: Fix these later
-    public static final double LAUNCHER_SET_PERCENTAGE_SLOW     = -0.53; // TODO: IMPORTANT! Tune this
-    public static final double LAUNCHER_SET_PERCENTAGE_MEDIUM   = -0.63; // TODO: IMPORTANT! Tune this
-    public static final double LAUNCHER_SET_PERCENTAGE_HIGH     = -0.70; // TODO: IMPORTANT! Tune this
-    public static final double LAUNCHER_SET_PERCENTAGE_CRITICAL = -0.80; // TODO: IMPORTANT! Tune this
-    public static final int RELEASE_MOTOR_RUNTIME               = 300; // In Milliseconds
-    //TODO: IMPORTANT! Tune these PID values later
-    // After reading information from Chief Delphi and RevRobotics people,
-    // these values are supposed to be insanely small as their
-    // software is different than most other PID controllers
-    public static final double LAUNCHER_P_VALUE                = 0.001;
-    public static final double LAUNCHER_I_VALUE                = 0.0000005;
-    public static final double LAUNCHER_D_VALUE                = 0.00001;
-
-    public static final double LAUNCHER_MAX_RPM                = 5700.0;
-    public static final double LAUNCHER_IZONE_VALUE            = 2500;
-
-    //////////////////////////////
-    // Intake Params
-    //////////////////////////////
-    public static final double INTAKE_MAX_SPEED                = 1.0;
-    public static final int INTAKE_ENCODER_PULSES              = 2048;
-    public static final double INTAKE_WHEEL_DIAMETER           = 5.08; // 2.0 Inches in centimetres
-    public static final boolean INTAKE_BALL_CHECK_INVERT       = false;
-
-    //////////////////////////////
-    // Ball Indexer Params
-    //////////////////////////////
-    public static final double INDEXER_SPEED                = 1.0;
-    public static final double LAUNCHER_FEEDER_SPEED        = 1.0;
-    public static final double TRIGGER_THRESHOLD            = 0.9;
-
-    ///////////////////////////////
-    // Climb Params
-    ///////////////////////////////
-    public static final double WINCH_MOTOR_SPEED            = 0.05;
-    public static final int WINCH_MOTOR_CURRENT_TRIGGER     = 7;    // Triggers current limit when the motor pulls 7A
-    public static final int WINCH_MOTOR_CURRENT_LIMIT       = 5;    // Sets the current limit to 5A
-    public static final double WINCH_MOTOR_TRIGGER_TIME     = 0.5;  // Time to trigger the motor current threshold
-    public static final boolean WINCH_LEFT_MOTOR_INVERT     = true; // TODO: Ensure motor direction is correct!
-
     ///////////////////////////////
     // Auto Params
     ///////////////////////////////
