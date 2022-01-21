0e6c9fc219aa15353d538282341cb288ff30a6c0
Dave Wilson <dave@webartz.com>
2022-01-20 18:36:29 -0600

    Add an empty ExampleSubsystem class

---
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -0,0 +1,40 @@
+package frc.robot.subsystems;
+
+public class ExampleSubsystem extends SKSubsystemBase
+{
+  /** Creates a new ExampleSubsystem. */
+  public ExampleSubsystem()
+  {
+
+  }
+
+  @Override
+  public void periodic()
+  {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic()
+  {
+    // This method will be called once per scheduler run during simulation
+  }
+
+  @Override
+  public void initializeTestMode()
+  {
+
+  }
+
+  @Override
+  public void testModePeriodic()
+  {
+
+  }
+
+  @Override
+  public void enterTestMode()
+  {
+
+  }
+}
