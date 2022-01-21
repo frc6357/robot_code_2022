83f103685a98f900c6febaf6c2b3c8895d91e46c
Jackson Craighead <ds020019jc@dsisdconnect.com>
2022-01-20 19:57:12 -0600

    created skeleton class for 2022 intake

---
+++ src/main/java/frc/robot/subsystems/SK22Intake.java
@@ -0,0 +1,71 @@
+//
+//SK22Intake: This is the system for getting cargo into the robot in the 2022 season.
+//
+
+package frc.robot.subsystems;
+
+
+public class SK22Intake extends SKSubsystemBase
+{
+  /** Creates a new SK22Intake. */
+  public SK22Intake()
+  {
+
+  }
+  //this should be the safe and initial code that happens. nothing should move
+  public void disableIntake()
+  {
+    
+  }
+  //this will extend the double solenoids, moving the intake to the active position
+  public void extendIntake()
+  {
+
+  }
+  //his will retract the double solenoids, moving the intake to the inactive position
+  public void retractIntake()
+  {
+
+  }
+  //this will set the speed of the motor that powers the ball pulling part of the intake
+  public void setIntakeSpeed()
+  {
+
+  }
+  //this will display the current speed of the motor
+  public void getIntakeSpeed()
+  {
+
+  }
+
+  @Override
+  public void periodic()
+  {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic()
+  {
+    // This method will be called once per scheduler run during simulation
+  }
+
+  @Override
+  public void initializeTestMode()
+  {
+
+  }
+
+  @Override
+  public void testModePeriodic()
+  {
+
+  }
+
+  @Override
+  public void enterTestMode()
+  {
+
+  }
+
+}
