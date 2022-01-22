6b703554dc5937187e41c621e56f1c9b8e15448a
Rushil_Ram <potatobervagesalad@gmail.com>
2022-01-22 13:26:23 -0600

    Updated

--- src/main/java/frc/robot/subsystems/SK22Drive.java
+++ src/main/java/frc/robot/subsystems/SK22Drive.java
@@ -3,22 +3,21 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.SPI;
 
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
-import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import frc.robot.Constants;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants.DriveConstants;
 import frc.robot.Ports;
-import frc.robot.TuningParams;
 import frc.robot.utils.MotorEncoder;
 
 public class SK22Drive extends SKSubsystemBase implements AutoCloseable
@@ -26,16 +25,16 @@ public class SK22Drive extends SKSubsystemBase implements AutoCloseable
     private final WPI_TalonFX leftLeader = new WPI_TalonFX(Ports.frontLeftDrive);
     private final WPI_TalonFX leftFollower = new WPI_TalonFX(Ports.backLeftDrive);
     private final MotorEncoder leftMotorEncoder =
-            new MotorEncoder(leftLeader, Constants.DriveConstants.kEncoderDistancePerPulse,
-                Constants.DriveConstants.kLeftEncoderReversed);
+            new MotorEncoder(leftLeader, DriveConstants.ENCODER_DISTANCE_PER_PULSE,
+                DriveConstants.LEFT_ENCODER_REVERSED);
     private final MotorControllerGroup leftGroup =
             new MotorControllerGroup(leftLeader, leftFollower);
 
     private final WPI_TalonFX rightLeader = new WPI_TalonFX(Ports.frontRightDrive);
     private final WPI_TalonFX rightFollower = new WPI_TalonFX(Ports.backRightDrive);
     private final MotorEncoder rightMotorEncoder =
-            new MotorEncoder(rightLeader, Constants.DriveConstants.kEncoderDistancePerPulse,
-                Constants.DriveConstants.kRightEncoderReversed);
+            new MotorEncoder(rightLeader, DriveConstants.ENCODER_DISTANCE_PER_PULSE,
+                DriveConstants.RIGHT_ENCODER_REVERSED);
     private final MotorControllerGroup rightGroup =
             new MotorControllerGroup(rightLeader, rightFollower);
 
@@ -69,7 +68,54 @@ public class SK22Drive extends SKSubsystemBase implements AutoCloseable
         rightGroup.setInverted(true);
 
         drive = new DifferentialDrive(leftGroup, rightGroup);
-        drive.setDeadband(TuningParams.DEADBAND_TURN);
+        drive.setDeadband(DriveConstants.DEADBAND_TURN);
+    }
+
+    @Override
+    public void periodic()
+    {
+        double leftEncoderDistanceMeters = leftMotorEncoder.getPositionMeters();
+        double rightEncoderDistanceMeters = rightMotorEncoder.getPositionMeters();
+        double leftEncoderSpeedMeters = leftMotorEncoder.getVelocityMeters();
+        double rightEncoderSpeedMeters = rightMotorEncoder.getVelocityMeters();
+        // Update the odometry in the periodic block
+        odometry.update(Rotation2d.fromDegrees(this.getHeading()), leftEncoderDistanceMeters,
+            rightEncoderDistanceMeters);
+
+        SmartDashboard.putNumber("Left Wheel Distance", leftEncoderDistanceMeters);
+        SmartDashboard.putNumber("Right Wheel Distance", rightEncoderDistanceMeters);
+        SmartDashboard.putNumber("Left Wheel Speed", leftEncoderSpeedMeters);
+        SmartDashboard.putNumber("Right Wheel Speed", rightEncoderSpeedMeters);
+        SmartDashboard.putNumber("Gyro Angle", this.getHeading());
+    }
+
+    public Pose2d getPose()
+    {
+        return odometry.getPoseMeters();
+    }
+
+    public DifferentialDriveWheelSpeeds getWheelSpeeds()
+    {
+        return new DifferentialDriveWheelSpeeds(leftMotorEncoder.getVelocityMeters(),
+            rightMotorEncoder.getVelocityMeters());
+    }
+
+    public void resetOdometry(Pose2d pose)
+    {
+        resetEncoders();
+        odometry.resetPosition(pose, Rotation2d.fromDegrees(gyro.getAngle()));
+    }
+
+    public void arcadeDrive(double fwd, double rot)
+    {
+        drive.arcadeDrive(fwd, rot);
+    }
+
+    public void tankDriveVolts(double leftVolts, double rightVolts)
+    {
+        leftGroup.setVoltage(leftVolts);
+        rightGroup.setVoltage(rightVolts);
+        drive.feed();
     }
 
     public void resetEncoders()
@@ -78,9 +124,35 @@ public class SK22Drive extends SKSubsystemBase implements AutoCloseable
         rightMotorEncoder.resetEncoder();
     }
 
+    public void resetGyro()
+    {
+        gyro.reset();
+    }
+
+    public double getAverageEncoderDistance()
+    {
+        // return (m_leftEncoder.getDistance() + m_rightEncoder.getDistance()) / 2.0;
+        return (leftMotorEncoder.getPositionMeters() + rightMotorEncoder.getPositionMeters()) / 2.0;
+    }
+
+    public void setMaxOutput(double maxOutput)
+    {
+        drive.setMaxOutput(maxOutput);
+    }
+
+    public void zeroHeading()
+    {
+        gyro.reset();
+    }
+
     public double getHeading()
     {
         return -gyro.getAngle();
+    }
+
+    public double getTurnRate()
+    {
+        return -gyro.getRate();
     }
 
     @Override
