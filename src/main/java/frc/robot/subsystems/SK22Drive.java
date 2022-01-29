eeb9ef1f068dead392bccd2508ebbdf1026fa9ab
Rushil_Ram <potatobervagesalad@gmail.com>
2022-01-29 14:48:50 -0600

    Added the gear shift into the code and comments

--- src/main/java/frc/robot/subsystems/SK22Drive.java
+++ src/main/java/frc/robot/subsystems/SK22Drive.java
@@ -1,3 +1,6 @@
+//
+// SK22Drive: Drivetrain subsystem for the Team 6357 2022 robot
+//
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
@@ -9,6 +12,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
@@ -18,8 +22,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Ports;
+import frc.robot.subsystems.base.SuperClasses.Gear;
 import frc.robot.utils.MotorEncoder;
 
+/**
+ * The SK22Drive class is the subsystem that controls the drive train of the Robot.
+ */
 public class SK22Drive extends SKSubsystemBase implements AutoCloseable
 {
     private final WPI_TalonFX leftLeader = new WPI_TalonFX(Ports.frontLeftDrive);
@@ -45,6 +53,8 @@ public class SK22Drive extends SKSubsystemBase implements AutoCloseable
 
     private SendableChooser<Boolean> testControlChooser = new SendableChooser<Boolean>();
 
+    private final DoubleSolenoid gearShiftSolenoid;
+
     private NetworkTableEntry leftLeaderEntry;
     private NetworkTableEntry leftFollowerEntry;
     private NetworkTableEntry rightLeaderEntry;
@@ -52,8 +62,10 @@ public class SK22Drive extends SKSubsystemBase implements AutoCloseable
     private NetworkTableEntry speedControllerGroupLeftEntry;
     private NetworkTableEntry speedControllerGroupRightEntry;
 
-    public SK22Drive()
+    /** Creates a new SK22Drive. */
+    public SK22Drive(DoubleSolenoid gearShiftSolenoid)
     {
+        this.gearShiftSolenoid = gearShiftSolenoid;
         resetEncoders();
         gyro.reset();
 
@@ -89,28 +101,73 @@ public class SK22Drive extends SKSubsystemBase implements AutoCloseable
         SmartDashboard.putNumber("Gyro Angle", this.getHeading());
     }
 
+    /**
+     * Returns the currently-estimated pose of the robot.
+     *
+     * @return The pose.
+     */
     public Pose2d getPose()
     {
         return odometry.getPoseMeters();
     }
 
+    /**
+     * Returns the current wheel speeds of the robot.
+     *
+     * @return The current wheel speeds.
+     */
     public DifferentialDriveWheelSpeeds getWheelSpeeds()
     {
         return new DifferentialDriveWheelSpeeds(leftMotorEncoder.getVelocityMeters(),
             rightMotorEncoder.getVelocityMeters());
     }
 
+    /**
+     * Resets the odometry to the specified pose.
+     *
+     * @param pose
+     *            The pose to which to set the odometry.
+     */
     public void resetOdometry(Pose2d pose)
     {
         resetEncoders();
         odometry.resetPosition(pose, Rotation2d.fromDegrees(gyro.getAngle()));
     }
 
+    /**
+     * Drives the robot using arcade controls.
+     *
+     * @param fwd
+     *            the commanded forward movement
+     * @param rot
+     *            the commanded rotation
+     */
     public void arcadeDrive(double fwd, double rot)
     {
         drive.arcadeDrive(fwd, rot);
     }
 
+    /**
+     * Drives the robot using tank controls
+     * 
+     * @param leftSpeed
+     *              the speed of the left motors
+     * @param rightSpeed
+     *              the speed of the right motors
+     */
+    public void tankDrive(double leftSpeed, double rightSpeed)
+    {
+        drive.tankDrive(leftSpeed, rightSpeed);
+    }
+
+    /**
+     * Controls the left and right sides of the drive directly with voltages.
+     * 
+     * @param leftVolts
+     *            the commanded left output
+     * @param rightVolts
+     *            the commanded right output
+     */
     public void tankDriveVolts(double leftVolts, double rightVolts)
     {
         leftGroup.setVoltage(leftVolts);
@@ -118,38 +175,63 @@ public class SK22Drive extends SKSubsystemBase implements AutoCloseable
         drive.feed();
     }
 
+    /** Resets the drive encoders to currently read a position of 0. */
     public void resetEncoders()
     {
         leftMotorEncoder.resetEncoder();
         rightMotorEncoder.resetEncoder();
     }
 
+    /** Reset the ADS gyro */
     public void resetGyro()
     {
         gyro.reset();
     }
 
+    /**
+     * Gets the average distance of the two encoders.
+     *
+     * @return the average of the two encoder readings
+     */
     public double getAverageEncoderDistance()
     {
         // return (m_leftEncoder.getDistance() + m_rightEncoder.getDistance()) / 2.0;
         return (leftMotorEncoder.getPositionMeters() + rightMotorEncoder.getPositionMeters()) / 2.0;
     }
 
+    /**
+     * Sets the max output of the drive. Useful for scaling the drive to drive more
+     * slowly.
+     *
+     * @param maxOutput
+     *            the maximum output to which the drive will be constrained
+     */
     public void setMaxOutput(double maxOutput)
     {
         drive.setMaxOutput(maxOutput);
     }
 
+    /** Zeroes the heading of the robot. */
     public void zeroHeading()
     {
         gyro.reset();
     }
 
+    /**
+     * Returns the heading of the robot.
+     *
+     * @return the robot's heading in degrees, from -180 to 180
+     */
     public double getHeading()
     {
         return -gyro.getAngle();
     }
 
+    /**
+     * Returns the turn rate of the robot.
+     *
+     * @return The turn rate of the robot, in degrees per second
+     */
     public double getTurnRate()
     {
         return -gyro.getRate();
@@ -185,6 +267,17 @@ public class SK22Drive extends SKSubsystemBase implements AutoCloseable
             .withSize(2, 1).withPosition(2, 3).getEntry();
     }
 
+    /**
+     * Sets the gear for both sides of the drivetrain.
+     * 
+     * @param newGear The gear value that we want the robot to achieve.
+     */
+    public void setGear(Gear newGear) 
+    {
+        DoubleSolenoid.Value check = (newGear == Gear.HIGH) ? DoubleSolenoid.Value.kForward: DoubleSolenoid.Value.kReverse;
+        gearShiftSolenoid.set(check);
+    }
+
     @Override
     public void testModePeriodic() 
     {
@@ -215,6 +308,10 @@ public class SK22Drive extends SKSubsystemBase implements AutoCloseable
         
     }
 
+    // The close() method is required to allow use with jUnit unit tests (see src/test/java).
+    // To use this, the class must implement the AutoCloseable interface.
+    // This is called to clean up between each test and must close all downstream objects
+    // before returning.
     @Override
     public void close() throws Exception {
         drive.close();
