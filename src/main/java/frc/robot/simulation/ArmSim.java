c4328c47c10864a2e596859a85aca31da31c17d8
Dave Wilson <ds020019jc@dsisdconnect.com>
2022-02-05 11:15:08 -0600

    Initial Arm Simulation

---
+++ src/main/java/frc/robot/simulation/ArmSim.java
@@ -0,0 +1,61 @@
+package frc.robot.simulation;
+
+public class ArmSim {
+
+    private final int baseLength;
+    private final int maxExtendLength;
+    private final int minAngle;
+    private final int maxAngle;
+    private double extendLength;
+    private double degreeLocation;
+
+    public ArmSim(int baseLength, int maxExtendLength, int minAngle, int maxAngle) {
+        this.baseLength = baseLength;
+        this.maxExtendLength = maxExtendLength;
+        this.minAngle = minAngle;
+        this.maxAngle = maxAngle;
+    }
+
+    public void setExtendLength(double length) {
+        if (length < 0) {
+            extendLength = 0;
+            return;
+        }
+        if (length > maxExtendLength) {
+            extendLength = maxExtendLength;
+            return;
+        }
+        extendLength = length;
+    }
+
+    public void changeExtendLength(double delta) {
+        setExtendLength(extendLength + delta);
+    }
+
+    public double getExtendLength() {
+        return extendLength;
+    }
+
+    public int getBaseLength() {
+        return baseLength;
+    }
+
+    public void changeAngle(double deltaDegrees) {
+        setAngle(degreeLocation + deltaDegrees);
+    }
+
+    private void setAngle(double newDegreeLocation) {
+        if (newDegreeLocation < minAngle) {
+            degreeLocation = minAngle;
+        } else if (newDegreeLocation > maxAngle) {
+            degreeLocation = maxAngle;
+        } else {
+            degreeLocation = newDegreeLocation;
+        }
+    }
+
+    public double getAngle() {
+        return degreeLocation;
+    }
+
+}
