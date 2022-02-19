eeb72edd2e7dd10d73f2f7f333a9514fd3071938
Dave Wilson <dave@webartz.com>
2022-02-18 18:17:01 -0600

    More warning removal

--- src/main/java/frc/robot/commands/subcommands/LowerComplexArmCommand.java
+++ src/main/java/frc/robot/commands/subcommands/LowerComplexArmCommand.java
@@ -2,12 +2,19 @@ package frc.robot.commands.subcommands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.SK22Climb;
-import frc.robot.subsystems.base.SimpleClimbArm;
 
-public class LowerComplexArmCommand extends CommandBase
+/**
+ * A command to lower the motor-controlled arm in the climb mechanism.
+ */
+class LowerComplexArmCommand extends CommandBase
 {
     private final SK22Climb climb;
 
+    /**
+     * Constructor for the complex arm lowering command.
+     * 
+     * @param climb The climb subsystem on which the command operates
+     */
     public LowerComplexArmCommand(SK22Climb climb)
     {
         this.climb = climb;
