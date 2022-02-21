92f8a92562ac85164eea0df9d7d98f3da6a35981
vivek-ramadhar <47376625+vivek-ramadhar@users.noreply.github.com>
2022-02-21 16:46:38 -0600

    changed vision algorithm for unideal scenarios

---
+++ Vision/scripts/Constants.py
@@ -0,0 +1,24 @@
+# target width in inches
+TARGET_WIDTH = 5
+
+# target height in inches
+TARGET_HEIGHT = 102
+
+# camera mounting height on the robot in inches
+MOUNTING_HEIGHT = 46.25
+
+# camera vertical angle offset in degrees from camera mount
+ANGLE_OFFSET = 40
+
+# target aspect ratio: width/height
+TARGET_ASPECT_RATIO = 5/2
+
+# the number of pixels detected in a vision target at the maximum range
+# DETERMINE THIS @ CALIBRATION BEFORE MATCH. THIS IS AN EMPIRICAL VALUE
+# for test purposes this is hard set to 30
+TARGET_MIN_AREA = 30
+
+# the number of pixels detected in a vision target at the minimum range
+# DETERMINE THIS @ CALIBRATION BEFORE MATCH. THIS IS AN EMPIRICAL VALUE
+# for test purposes this is hard set to 500
+TARGET_MAX_AREA = 500
