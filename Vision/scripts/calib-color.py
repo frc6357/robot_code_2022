9d2bc2923fbc344162e7266e207c275188c5f62d
Vivek Ramadhar <vivek.ramadhar@gmail.com>
2022-03-30 19:31:08 +0000

    added camera calibration methods to calib-color for checkerboard pattern images

--- Vision/scripts/calib-color.py
+++ Vision/scripts/calib-color.py
@@ -1,6 +1,75 @@
 import PIL.Image
 import cv2
+import os
+import glob
+import numpy
 
+def cameraCalib(patternDim):
+    # vector to store 3D points within the real world for checkerboard image
+    objpoints = []
+    # vector to store 2D points within the image for checkerboard image
+    imgpoints = []
+    
+    # termination criteria for iterative method, cornerSubPix
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    
+    # defining real world conditions for 3D points
+    objp = = np.zeros((1, patternDim[0] * patternDim[1], 3), np.float32)
+    objp[0,:,:2] = np.mgrid[0:patternDim[0], 0:patternDim[1]].T.reshape(-1,2)
+    prev_img_shape = None
+    
+    # Extractingpath of each image in a directory
+    images = glob.glob('./camera-calib-images/*.jpg')
+    for image in images:
+        img = cv2.imread(image)
+        gray = cv2.cvtColor(cv2.COLOR_BGR2GRAY)
+        
+        # find chess board corners; ret = True if desired number of corners 
+        # are found 
+        ret, corners = cv2.findChessboardCorners(gray, patternDim, 
+        cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + 
+        cv2.CALIB_CB_NORMALIZE_IMAGE)
+        
+        # if desired number of corners are found
+        if ret == True:
+            objpoints.append(objp)
+            
+            # using cv2 functions to increase precision of pixel corners selected
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1,-1), criteria)
+            
+            imgpoints.append(corners2)
+            
+            # draw and display corners
+            img = cv2.drawChessboardCorners(img, CheckerBoard, corners2, ret)
+            
+        cv2.imshow('frame', img)
+        cv2.waitKey(0)
+    cv2.destroyAll Windows()
+    h,w = img.shape[:2]
+    
+    # this returns in the following order:
+    # retval (True or False)
+    # mtx (contains the intrinsic parameters of the camera -> *should be* 3 x 3)
+    # dist (contains the lens distortion coefficients)
+    # rvecs (contains the rotation vector -> 3 x 1)
+    # tvecs (contains the translation vector -> 3 x 1)
+    return cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+def undistort(imgParam, mtx, dist):
+    img = cv2.imread(imgParam)
+    h, w = img.shape[:2]
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+    
+    # undistort image
+    dst = cv2.undistort(img, mtx, dist, None, newcamermtx)
+    
+    # crop the image
+    x, y, w ,h = roi
+    dst = dst[y:y+h, x:x+w]
+    
+    # returns the undistorted image
+    return dst
+    
 def rgb_to_hsv(input):
     r, g, b = input
     r, g, b = r/255.0, g/255.0, b/255.0
@@ -46,4 +115,7 @@ cv2.imshow("Input Frame", img)
 
 if cv2.waitKey(0) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
+                
+if __name__ == "__main__":
+    
 
