486f31fa9baa7e42020970faf848c58731f5ae45
salemgeorge <salem@georgefour.com>
2022-11-09 15:43:45 -0600

    Found the zero values for the four CANCoders

--- users/examples/SwerveDrive/src/main/java/frc/robot/Constants.java
+++ users/examples/SwerveDrive/src/main/java/frc/robot/Constants.java
@@ -5,18 +5,22 @@ public class Constants {
     public static int FRONT_LEFT_TURNING_CAN_ID     = 20;
     public static int FRONT_LEFT_DRIVING_CAN_ID     = 10;
     public static int FRONT_LEFT_CANCODER_ID        = 30;
+    // CAN Coder zero value = 237.129
 
     public static int FRONT_RIGHT_TURNING_CAN_ID    = 22;
     public static int FRONT_RIGHT_DRIVING_CAN_ID    = 12;
     public static int FRONT_RIGHT_CANCODER_ID       = 32;
+    // CAN coder zero value = 90.703
 
     public static int BACK_LEFT_TURNING_CAN_ID      = 21;
     public static int BACK_LEFT_DRIVING_CAN_ID      = 11;
     public static int BACK_LEFT_CANCODER_ID         = 31;
+    // CAN Coder zero value = 289.951
 
     public static int BACK_RIGHT_TURNING_CAN_ID     = 23;
     public static int BACK_RIGHT_DRIVING_CAN_ID     = 13;
     public static int BACK_RIGHT_CANCODER_ID        = 33;
+    // CAN Coder zero value = 66.709
 
     public static final double kWheelRadius         = 0.0508;
     public static final int kDriveEncoderResolution = 4096;
